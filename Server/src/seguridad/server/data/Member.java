ad85b07365574293fa56457cefe613262810e535
Carlos Perales <Carlos Perales@carlos-lap>
2016-04-12 10:16:57 +0200

    v0.0(Cliente vacío)

---
+++ Server/src/seguridad/server/data/Member.java
@@ -0,0 +1,85 @@
+package seguridad.server.data;
+
+import java.util.*;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class Member {
+	
+	@PrimaryKey
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private Date birthdate;
+	private String address;
+	private String country;
+
+ 
+
+	public Member(String username,String password, String name, String email, Date birthdate, String address, String country,String billingInfo, String paymentMethod) {
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.birthdate = birthdate;
+		this.address = address;
+		this.country = country;
+
+	}
+
+	public String toString(){
+		return this.username+" "+this.password+" "+this.name+" "+this.email+" "+this.birthdate+" "+this.address+" "+this.country+" ";
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setPassword(String password){
+		this.password = password;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public void setBirthdate(Date birthdate){
+		this.birthdate = birthdate;
+	}
+
+	public void setAddress(String address){
+		this.address = address;
+	}
+
+	public void setCountry(String country){
+		this.country = country;
+	}
+
+	public String getUsername(){
+		return this.username;
+	}
+	
+	public String getPassword(){
+		return this.password;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public String getEmail(){
+		return this.email;
+	}
+
+	public String getAddress(){
+		return this.address;
+	}
+
+	public String getCountry(){
+		return this.country;
+	}
+
+}
